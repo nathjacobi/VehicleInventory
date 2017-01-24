d471053f946ea967b4f0665a0cff598ff5d1363f
Nathaniel Jacobi <njacobi@mines.edu>
2017-01-24 16:59:24 -0700

    Made console application and has reference to library

---
+++ VehicleInventoryConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleInventory;
+
+namespace VehicleInventoryConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle car1 = new Vehicle();
+            Console.WriteLine("Hello world!");
+            Console.ReadLine();
+        }
+    }
+}
