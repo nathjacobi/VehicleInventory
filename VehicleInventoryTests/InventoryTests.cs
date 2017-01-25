3f18a9c422526547f9ec117f0bf9309f5f7f145b
Nathaniel Jacobi <njacobi@mines.edu>
2017-01-25 13:52:42 -0700

    Added the files for the Inventory class and its Unit tests

---
+++ VehicleInventoryTests/InventoryTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VehicleInventoryTests
+{
+    [TestClass]
+    public class InventoryTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
