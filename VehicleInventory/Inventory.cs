ef5699a75e662afffdfafe3d670ea470b9ba8b8b
Nathaniel Jacobi <njacobi@mines.edu>
2017-01-25 14:23:48 -0700

    Added skeleton functions for what I want to implement

--- VehicleInventory/Inventory.cs
+++ VehicleInventory/Inventory.cs
@@ -8,5 +8,30 @@ namespace VehicleInventory
 {
     class Inventory
     {
+        List<Vehicle> vehicleList;
+        public Inventory()
+        {
+            vehicleList = new List<Vehicle>();
+        }
+
+        public void AddVehicle(Vehicle car)
+        {
+
+        }
+
+        public void RemoveVehicle(Vehicle car)
+        {
+
+        }
+
+        public List<Vehicle> GetVehicleList()
+        {
+            return new List<Vehicle>();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
