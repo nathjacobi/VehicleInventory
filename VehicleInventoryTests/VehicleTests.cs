52981bdef82f30c9653df3fd131ae154b4d5b909
Nathaniel Jacobi <njacobi@mines.edu>
2017-01-24 17:28:34 -0700

    Made unit tests for the project so I can test as I go

---
+++ VehicleInventoryTests/VehicleTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VehicleInventory;
+
+namespace VehicleInventoryTests
+{
+    [TestClass]
+    public class VehicleTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
