29a0037f4e36c561ec141bd9459d88dc7b5aa123
Nathaniel Jacobi <njacobi@mines.edu>
2017-01-26 16:13:38 -0700

    Added the Find by year/make functions and add to mileage

--- VehicleInventory/GlobalData.cs
+++ VehicleInventory/GlobalData.cs
@@ -14,5 +14,67 @@ namespace VehicleInventory
         {
             ManufacturerDictionary.Add(maker.Name, maker);
         }
+
+        public static List<Vehicle> FindByYear(int givenYear, List<Vehicle> givenList)
+        {
+            List<Vehicle> matchingYear = new List<Vehicle>();
+            foreach (Vehicle car in givenList)
+            {
+                if (car.Year == givenYear)
+                    matchingYear.Add(car);
+            }
+            return matchingYear;
+        }
+
+        public static List<Vehicle> FindByYear(int givenYear, Inventory givenInventory)
+        {
+            List<Vehicle> matchingYear = new List<Vehicle>();
+            foreach (Vehicle car in givenInventory.GetVehicleList())
+            {
+                if (car.Year == givenYear)
+                    matchingYear.Add(car);
+            }
+            return matchingYear;
+        }
+
+        public static List<Vehicle> FindByMake(string givenMake, List<Vehicle> givenList)
+        {
+            List<Vehicle> matchingMake = new List<Vehicle>();
+            foreach (Vehicle car in givenList)
+            {
+                if (car.Make == givenMake)
+                    matchingMake.Add(car);
+            }
+            return matchingMake;
+        }
+
+        public static List<Vehicle> FindByMake(string givenMake, Inventory givenInventory)
+        {
+            List<Vehicle> matchingMake = new List<Vehicle>();
+            foreach (Vehicle car in givenInventory.GetVehicleList())
+            {
+                if (car.Make == givenMake)
+                    matchingMake.Add(car);
+            }
+            return matchingMake;
+        }
+
+        public static List<Vehicle> AddToMileage(int additionalMileage, List<Vehicle> givenList)
+        {
+            foreach (Vehicle car in givenList)
+            {
+                car.Mileage += additionalMileage;
+            }
+            return givenList;
+        }
+
+        public static List<Vehicle> AddToMileage(int additionalMileage, Inventory givenInventory)
+        {
+            foreach (Vehicle car in givenInventory.GetVehicleList())
+            {
+                car.Mileage += additionalMileage;
+            }
+            return givenInventory.GetVehicleList();
+        }
     }
 }
