d5d6466baa244e8a3e1a40822191c66146df44ae
Nathaniel Jacobi <njacobi@mines.edu>
2017-01-26 15:46:50 -0700

    Added the global data files

---
+++ VehicleInventory/GlobalData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleInventory
+{
+    public static class GlobalData
+    {
+        public static Dictionary<string, Manufacturer> ManufacturerDictionary { get; set; }
+
+        public static void AddManufactuer(Manufacturer maker)
+        {
+            ManufacturerDictionary.Add(maker.Name, maker);
+        }
+    }
+}
