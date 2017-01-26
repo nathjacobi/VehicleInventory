3e6fed6fa9494160b2cd65f0656ef8f4e3aec9da
Nathaniel Jacobi <njacobi@mines.edu>
2017-01-25 17:35:49 -0700

    Made the Manu constructor and getters and setters for attributes

--- VehicleInventory/Manufacturer.cs
+++ VehicleInventory/Manufacturer.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace VehicleInventory
 {
-    class Manufacturer
+    public class Manufacturer
     {
+        private string name;
+        private string mainAddress;
+        private string phoneNumber;
+
+        public Manufacturer(string name, string mainAddress, string phoneNumber)
+        {
+            this.name = name;
+            this.mainAddress = mainAddress;
+            this.phoneNumber = phoneNumber;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string MainAddress
+        {
+            get { return mainAddress; }
+            set { mainAddress = value; }
+        }
+
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; }
+        }
     }
 }
