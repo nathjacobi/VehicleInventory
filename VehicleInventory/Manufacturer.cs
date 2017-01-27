ab9336f95c61b57f5439f79ae74adff1a9adef66
Nathaniel Jacobi <njacobi@mines.edu>
2017-01-27 09:23:54 -0700

    Added the attributes for the oil and ways to alter it if more special cases come up

--- VehicleInventory/Manufacturer.cs
+++ VehicleInventory/Manufacturer.cs
@@ -9,43 +9,61 @@ namespace VehicleInventory
     public class Manufacturer
     {
         //Attributes of a Manufacturer
-        private string name;
-        private string mainAddress;
-        private string phoneNumber;
+        private string Name { get; set; }
+        private string MainAddress { get; set; }
+        private string PhoneNumber { get; set; }
+        private bool NeedsOilChange { get; set; }
+        private int MilesPerOilChange { get; set; }
+        private int DaysPerOilChange { get; set; }
 
         //Constructor for Manufacturer. Takes in the name, address, and phone number
+        //Also sets the oil requirements. Automatically sets the Subaru, VW, and Tesla Special conditions
         public Manufacturer(string name, string mainAddress, string phoneNumber)
         {
-            this.name = name;
-            this.mainAddress = mainAddress;
-            this.phoneNumber = phoneNumber;
+            Name = name;
+            MainAddress = mainAddress;
+            PhoneNumber = phoneNumber;
+
+            //If its a Subaru, sets the oil requirements to its special condition
+            if (name == "Subaru")
+            {
+                NeedsOilChange = true;
+                MilesPerOilChange = 7000;
+                DaysPerOilChange = 180;
+            }
+            //If its a Volkswagen, sets the oil requirements to its special condition
+            else if (name == "Volkswagen")
+            {
+                NeedsOilChange = true;
+                MilesPerOilChange = 8000;
+                DaysPerOilChange = 210;
+            }
+            //If its a Tesla, sets the oil requirements to its special condition
+            else if (name == "Tesla")
+            {
+                NeedsOilChange = false;
+            }
+            //Anything else has its oil requirements set to the default
+            else
+            {
+                NeedsOilChange = true;
+                MilesPerOilChange = 3000;
+                DaysPerOilChange = 90;
+            }
         }
 
-        //Get set for name
-        public string Name
+        //Function to change the oil requirements for any Manufacturer in case more special conditions pop up
+        public void changeManufacturerOilRequirements(bool needOilChange, int milesPerChange, int daysPerChange)
         {
-            get { return name; }
-            set { name = value; }
-        }
-
-        //Get set for mainAddress
-        public string MainAddress
-        {
-            get { return mainAddress; }
-            set { mainAddress = value; }
-        }
-
-        //Get set for phoneNumber
-        public string PhoneNumber
-        {
-            get { return phoneNumber; }
-            set { phoneNumber = value; }
+            NeedsOilChange = needOilChange;
+            MilesPerOilChange = milesPerChange;
+            DaysPerOilChange = daysPerChange;
         }
 
         //Returns a Manufacturer object's info as a formatted string
         public override string ToString()
         {
-            return String.Format("Name: {0}, Main Address: {1}, Telephone: {2}\n", name, mainAddress, phoneNumber);
+            return String.Format("Name: {0}, Main Address: {1}, Telephone: {2}\n", Name, MainAddress, PhoneNumber);
         }
     }
 }
